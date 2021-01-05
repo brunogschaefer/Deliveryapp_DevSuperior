47e4f15e6cfcfa74702fd901bfd57c98ff7d2d4d
Bruno Schaefer <brunogschaefer@outlook.com>
2021-01-05 01:11:51 -0300

    Domain model

--- backend_delivery/src/main/java/com/Bruno/delivery/DeliveryApplication.java
+++ backend_delivery/src/main/java/com/Bruno/delivery/DeliveryApplication.java
@@ -2,12 +2,14 @@ package com.Bruno.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class DeliveryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeliveryApplication.class, args);
+
 	}
 
 }
---
+++ backend_delivery/src/main/java/com/Bruno/delivery/entities/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.Bruno.delivery.entities;
+
+public enum OrderStatus {
+	
+	PENDING, DELIVERED; 
+
+}
