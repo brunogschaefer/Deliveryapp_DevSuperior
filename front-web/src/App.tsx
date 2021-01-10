8ab5d93f787bb6f99dc308c7fd85a28aec385d29
Bruno Schaefer <brunogschaefer@outlook.com>
2021-01-10 17:22:37 -0300

    Finished front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import Routes from './Routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <Routes />
+    <>
+      <Routes />
+      <ToastContainer />
+    </>
   );
 }
 
--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { OrderPayLoad } from "./Orders/types";
 
 const API_URL = 'https://schefa-delivery.herokuapp.com';
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
@@ -9,4 +10,8 @@ export function fetchProducts(){
 export function fetchLocalMapbox(local: string) {
     return axios(`
     https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+}
+
+export function saveOrder(payload: OrderPayLoad) {
+    return axios.post(`${API_URL}/orders`, payload);
 }
